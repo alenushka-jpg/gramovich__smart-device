934ec5e535c188f6d9c8f595a6dd6ce27c05a917
alenushka <alionxlo@gmail.com>
2021-12-12 12:37:35 +0300

    Дорабатывает модальное окно

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,27 @@
-// комментарий
+'use strict';
+
+(function () {
+// Открытие модального окна по кнопке Заказать звонок
+  var openModal = document.querySelector('.button-open');
+  var closeModal = document.querySelector('.button-close');
+  var modalWindow = document.querySelector('.modal-window');
+
+  function showWindow() {
+    modalWindow.classList.add('modal-window--show');
+  }
+
+  function hideModal() {
+    modalWindow.classList.remove('modal-window--show');
+  }
+
+  function onOpenClick() {
+    showWindow()
+  }
+
+  function onCloseClick() {
+    hideModal()
+  }
+
+  openModal.addEventListener('click', onOpenClick);
+  // closeModal.addEventListener('click', onCloseClick);
+})();
