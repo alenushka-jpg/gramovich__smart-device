2207e488f7ac9d64fdb0a87c277805ab389b52a9
alenushka <alionxlo@gmail.com>
2021-12-13 13:28:26 +0300

    Добавляет маску на номер телефона

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,7 @@
   var closeModal = document.querySelector('.modal-window__button-close');
   var modalWindow = document.querySelector('.modal-window');
   var username = modalWindow.querySelector('[name=username]');
+  var userForm = document.querySelector('#form-user');
 
   function showWindow() {
     modalWindow.classList.add('modal-window--show');
@@ -37,9 +38,9 @@
 
   // local storage
   var phone = modalWindow.querySelector('[name=phone]');
-  var form = modalWindow.querySelector('#form-popup');
+  var formPopup = modalWindow.querySelector('#form-popup');
 
-  form.addEventListener('submit', function (evt) {
+  formPopup.addEventListener('submit', function (evt) {
     if (!username.value || !phone.value) {
       evt.preventDefault();
     } else {
@@ -47,4 +48,23 @@
       localStorage.setItem('phone', phone.value);
     }
   });
+
+  // Маска на номер телефона
+  const Inputmask = window.Inputmask;
+
+  if (formPopup) {
+    const telInput = formPopup.querySelector('#tel-input');
+    const mask = new Inputmask('+7(999)999-99-99');
+    if (telInput) {
+      mask.mask(telInput);
+    }
+  }
+
+  if (userForm) {
+    const tel = userForm.querySelector('#tel');
+    const mask = new Inputmask('+7(999)999-99-99');
+    if (tel) {
+      mask.mask(tel);
+    }
+  }
 })();
